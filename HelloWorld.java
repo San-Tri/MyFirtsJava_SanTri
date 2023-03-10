1015d01c5459fc98e0dc19cea0d59937bf73c8a4
San-Tri <1473024@senati.pe>
2023-03-10 07:24:05 -0500

    algoritmo.1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
-<h1>
-    <body stylen="color:red"
-    hello world SENATI
-    SanTri
-</h1>
+public class App {
+    public static void main(String[] args) throws Exeption {
+System.out.println("HOLA MUNDO");
+System.out.println("soy SanTri");        
+    }
+
+}
